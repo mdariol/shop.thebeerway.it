ea1c3c4839f7818a6909e75e5cef1836fc50c016
Matteo Palazzo <outlaw.plz@gmail.com>
2019-05-07 15:45:00 +0200

    Generate Beer resource

---
+++ app/Http/Controllers/BeerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Beer;
+use Illuminate\Http\Request;
+
+class BeerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Beer  $beer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Beer $beer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Beer  $beer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Beer $beer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Beer  $beer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Beer $beer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Beer  $beer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Beer $beer)
+    {
+        //
+    }
+}
