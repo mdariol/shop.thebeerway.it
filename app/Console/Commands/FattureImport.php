d906fc1a2507937a4c775e7f11791ee5565f7792
OutlawPlz <outlaw.plz@gmail.com>
2019-06-19 17:46:10 +0200

    Improves fatture:import output

--- app/Console/Commands/FattureImport.php
+++ app/Console/Commands/FattureImport.php
@@ -9,7 +9,6 @@ use App\Packaging;
 use App\Services\FattureInCloud;
 use App\Style;
 use Illuminate\Console\Command;
-use Illuminate\Support\Collection;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 
 class FattureImport extends Command
@@ -36,19 +35,17 @@ class FattureImport extends Command
     /**
      * Create a new command instance.
      *
-     * @return void
+     * @param  \App\Services\FattureInCloud  $fattureInCLoud
      */
-    public function __construct(FattureInCloud $fatture_in_cloud)
+    public function __construct(FattureInCloud $fattureInCLoud)
     {
         parent::__construct();
 
-        $this->fattureInCLoud = $fatture_in_cloud;
+        $this->fattureInCLoud = $fattureInCLoud;
     }
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
     public function handle()
     {
@@ -58,14 +55,26 @@ class FattureImport extends Command
             throw new InvalidArgumentException("Data \"$data\" is not defined.");
         }
 
-        $this->$data();
+        $skipped = $this->$data();
+        $this->footer($skipped);
     }
 
+    /**
+     * Reads beers from Fatture in Cloud products and try to import them into
+     * the application.
+     *
+     * @return array
+     */
     protected function beers()
     {
         $beers = $this->fattureInCLoud->parseBeers();
 
-        $beers->each(function ($beer) {
+        $bar = $this->output->createProgressBar(count($beers));
+        $bar->start();
+
+        $skipped = [];
+
+        $beers->each(function ($beer) use ($bar, &$skipped) {
             try {
                 Beer::updateOrCreate(['code' => $beer->code], $beer->toArray() + [
                     'brewery_id' => Brewery::firstOrCreate($beer->brewery->toArray())->id,
@@ -74,60 +83,149 @@ class FattureImport extends Command
                     'packaging_id' => Packaging::firstOrCreate($beer->packaging->toArray())->id,
                 ]);
             } catch (\Exception $exception) {
-                $this->error("Cannot import beer \"$beer->name\" with code \"$beer->code\"");
+                $skipped[] = $beer;
             }
+
+            $bar->advance();
         });
+
+        $bar->finish();
+
+        return $skipped;
     }
 
+    /**
+     * Reads breweries from Fatture in Cloud products and try to import them
+     * into the application.
+     *
+     * @return array
+     */
     protected function breweries()
     {
         $breweries = $this->fattureInCLoud->parseBreweries();
 
-        $breweries->each(function ($brewery) {
+        $bar = $this->output->createProgressBar(count($breweries));
+        $bar->start();
+
+        $skipped = [];
+
+        $breweries->each(function ($brewery) use ($bar, &$skipped) {
             try {
                 Brewery::firstOrCreate($brewery->toArray());
             } catch (\Exception $exception) {
-                $this->error("Cannot import brewery \"$brewery->name\"");
+                $skipped[] = $brewery;
+                // $this->error("Cannot import brewery \"$brewery->name\"");
             }
+
+            $bar->advance();
         });
+
+        $bar->finish();
+
+        return $skipped;
     }
 
+    /**
+     * Reads packagings from Fatture in Cloud products and try to import them
+     * into the application.
+     */
     protected function packagings()
     {
         $packagings = $this->fattureInCLoud->parsePackagings();
 
-        $packagings->each(function ($packaging) {
+        $bar = $this->output->createProgressBar(count($packagings));
+        $bar->start();
+
+        $skipped = [];
+
+        $packagings->each(function ($packaging) use ($bar, &$skipped) {
             try {
                 Packaging::firstOrCreate($packaging->toArray());
             } catch (\Exception $exception) {
-                $this->error("Cannot import packaging \"$packaging->name\"");
+                $skipped[] = $packaging;
             }
+
+            $bar->advance();
         });
+
+        $bar->finish();
+
+        return $skipped;
     }
 
+    /**
+     * Reads styles from Fatture in Cloud products and try to import them into
+     * the application.
+     */
     protected function styles()
     {
         $styles = $this->fattureInCLoud->parseStyles();
 
-        $styles->each(function ($style) {
+        $bar = $this->output->createProgressBar(count($styles));
+        $bar->start();
+
+        $skipped = [];
+
+        $styles->each(function ($style) use ($bar, &$skipped) {
             try {
                 Style::firstOrCreate($style->toArray());
             } catch (\Exception $exception) {
-                $this->error("Cannot import style \"$style->name\"");
+                $skipped[] = $style;
             }
+
+            $bar->advance();
         });
+
+        $bar->finish();
+
+        return $skipped;
     }
 
+    /**
+     * Reads colors from Fatture in Cloud products and try to import them into
+     * the application.
+     */
     protected function colors()
     {
         $colors = $this->fattureInCLoud->parseColors();
 
-        $colors->each(function ($color) {
+        $bar = $this->output->createProgressBar(count($colors));
+        $bar->start();
+
+        $skipped = [];
+
+        $colors->each(function ($color) use ($bar, &$skipped) {
             try {
                 Color::firstOrCreate($color->toArray());
             } catch (\Exception $exception) {
-                $this->error("Cannot import color \"$color->name\"");
+                $skipped[] = $color;
             }
+
+            $bar->advance();
         });
+
+        $bar->finish();
+
+        return $skipped;
+    }
+
+    /**
+     * Prints sync results.
+     *
+     * @param array $skipped
+     */
+    protected function footer(array $skipped)
+    {
+        $data = $this->argument('data');
+
+        $this->info("\n\nImported $data successfully!\n");
+
+        if ($count = count($skipped)) {
+            $this->line("Skipped elements ($count):\n");
+
+            foreach ($skipped as $item) {
+                $this->line("  - $item->name");
+            }
+        }
     }
 }
