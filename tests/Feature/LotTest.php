ef26ee98beb39276d7805026578b0c080f820ae0
OutlawPlz <outlaw.plz@gmail.com>
2019-12-17 11:13:27 +0100

    Testing Lot permissions

--- tests/Feature/LotTest.php
+++ tests/Feature/LotTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Lot;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,15 +18,58 @@ class LotTest extends TestCase
     {
         parent::setUp();
 
-        $this->lot = factory(Lot::class)->create([
-            'stock' => 5,
-            'reserved' => 7,
-        ]);
+        $this->lot = factory(\App\Lot::class)->create();
     }
 
     /** @test */
-    public function a_lot_can_be_unavailable()
+    public function unauthorized_user_cannot_view_lots()
     {
-        $this->assertFalse($this->lot->isAvailable());
+        $responses = collect([]);
+
+        $responses->push($this->get(route('admin.lots.index')));
+        $responses->push($this->get(route('admin.lots.show', ['lot' => $this->lot])));
+
+        $responses->each(function ($response) {
+            $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        });
+    }
+
+    /** @test */
+    public function unauthorized_user_cannot_create_lots()
+    {
+        $responses = collect([]);
+
+        $responses->push($this->get(route('admin.lots.create')));
+        $responses->push($this->post(route('admin.lots.store')));
+
+        $responses->each(function ($response) {
+            $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        });
+    }
+
+    /** @test */
+    public function unauthorized_user_cannot_edit_lots()
+    {
+        $responses = collect([]);
+
+        $responses->push($this->get(route('admin.lots.edit', ['lot' => $this->lot])));
+        $responses->push($this->patch(route('admin.lots.update', ['lot' => $this->lot])));
+
+        $responses->each(function ($response) {
+            $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        });
+    }
+
+    /** @test */
+    public function unauthorized_user_cannot_delete_lots()
+    {
+        $responses = collect([]);
+
+        $responses->push($this->get(route('admin.lots.delete', ['lot' => $this->lot])));
+        $responses->push($this->delete(route('admin.lots.destroy', ['lot' => $this->lot])));
+
+        $responses->each(function ($response) {
+            $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        });
     }
 }
